58f7e02f1f4764ff2c02f26fd0fc9c669568b622
Lucian Dumitru <lucian.dumitru@gmail.com>
2021-05-07 17:57:45 +0300

    Solve ua-parser-js vulnerability issue

---
+++ sidebars_new.js
@@ -0,0 +1,98 @@
+module.exports = {
+  someSidebar: {
+    'Fizica': [
+                {
+                    'Clasa 6' : [
+                        {
+                            'I.Introducere în studiul fizicii' : [
+                                'fizica/clasa6/fizica-clasa6-capitolul1',
+                            ]
+
+
+
+                        },
+                        'fizica/clasa6/fizica-clasa6-capitolul1',
+                        'fizica/clasa6/fizica-clasa6-capitolul2',
+                        'fizica/clasa6/fizica-clasa6-capitolul3',
+                        'fizica/clasa6/fizica-clasa6-capitolul4',
+                        'fizica/clasa6/fizica-clasa6-capitolul5',
+                        'fizica/clasa6/fizica-clasa6-capitolul6'
+                    ],
+                },
+                {
+                    'Clasa 7' : [
+                        'fizica/clasa7/fizica-clasa7-capitolul1',
+                        'fizica/clasa7/fizica-clasa7-capitolul2',
+                        'fizica/clasa7/fizica-clasa7-capitolul3',
+                        'fizica/clasa7/fizica-clasa7-capitolul4',
+                        'fizica/clasa7/fizica-clasa7-capitolul5',
+                        'fizica/clasa7/fizica-clasa7-capitolul6'
+                    ],
+                },
+                {
+                    'Clasa 8' : [
+                        'fizica/clasa8/fizica-clasa8-capitolul1',
+                        'fizica/clasa8/fizica-clasa8-capitolul2',
+                        'fizica/clasa8/fizica-clasa8-capitolul3',
+                        'fizica/clasa8/fizica-clasa8-capitolul4'
+                    ],
+                },
+                {
+                   'Ghidul de fizica': [
+                       'fizica/ghidul-de-fizica-versus-accidentari'
+                   ],
+                },
+                {
+                   'Experimente distractive': [
+                       'fizica/experimente-distractive-fizica'
+                   ],
+                },
+                {
+                   'Blibliografie Fizică': [
+                       'fizica/bibliografie_fizica'
+                   ],
+                },
+               ],
+     'Chimie': [
+                {
+                    'Clasa 7' : [
+                        'chimie/clasa7/chimie-clasa7-capitolul1',
+                        'chimie/clasa7/chimie-clasa7-capitolul2',
+                        'chimie/clasa7/chimie-clasa7-capitolul3',
+                        'chimie/clasa7/chimie-clasa7-capitolul4',
+                        'chimie/clasa7/chimie-clasa7-capitolul5',
+                        'chimie/clasa7/chimie-clasa7-capitolul6',
+                        'chimie/clasa7/chimie-clasa7-capitolul7'
+                    ],
+
+                },
+                {
+
+                    'Clasa 8' : [
+                        'chimie/clasa8/chimie-clasa8-capitolul1',
+                        'chimie/clasa8/chimie-clasa8-capitolul2',
+                        'chimie/clasa8/chimie-clasa8-capitolul3',
+                        'chimie/clasa8/chimie-clasa8-capitolul4',
+                        'chimie/clasa8/chimie-clasa8-capitolul5',
+                        'chimie/clasa8/chimie-clasa8-capitolul6',
+                        'chimie/clasa8/chimie-clasa8-capitolul7'
+                    ],
+                },
+                {
+                    'Ghidul de chimie': [
+                        'chimie/ghidul-de-chimie-versus-accidentari'
+                    ],
+                },
+                {
+                     'Experimente distractive': [
+                         'chimie/experimente-distractive-chimie'
+                     ],
+                },
+                {
+                     'Blibliografie Chimie': [
+                         'chimie/bibliografie_chimie'
+                     ],
+                },
+                ],
+  },
+};
